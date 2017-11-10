aac0ab21d4cabab63d8f7bbfd834c6f5f99232ee
Evaisa Giac <evagiacosa1@gmail.com>
2017-11-10 15:33:31 +0100

    hotfix

--- utils/HTTPServer.js
+++ utils/HTTPServer.js
@@ -60,8 +60,8 @@ function HTTPServer(app) {
 
   server.use(function (err, req, res, next) {
 	var ip = req.ip || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-    if (ip == '86.82.111.67') res.end();
-    app.log.error(err);
+    //if (ip == '86.82.111.67') res.end();
+    //app.log.error(err);
   });
 
   return {
