57b91a502d01cffcb0e08eedb0499adaae223fa2
Evaisa Giac <evagiacosa1@gmail.com>
2017-11-25 14:49:42 +0100

    login to draw

--- utils/WebsocketServer.js
+++ utils/WebsocketServer.js
@@ -156,7 +156,10 @@ function WebsocketServer(app) {
       // Tried to place before their cooldown has expired, ignore
       let now = Date.now();
       if (typeof ipClients[ip].cooldown === 'undefined' || ipClients[ip].cooldown - now > 0 && !clients[id].is_moderator) return;
-	  
+	  if (anon == true) {
+		  socket.emit('alert', 'You need to login to draw');
+		  return;
+	  }
 	/*	if (!clients[id].is_registered) {
 		  socket.emit('alert', 'You need to login to draw');
 		  return;
